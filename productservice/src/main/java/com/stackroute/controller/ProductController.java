50309b5852a2fe3e257f5bfb557d29998ae3c9d9
jia.mo <jia.mo@cgi.com>
2021-08-18 00:22:29 +0530

    Update productservice/src/main/java/com/stackroute/controller/ProductController.java

---
+++ productservice/src/main/java/com/stackroute/controller/ProductController.java
@@ -0,0 +1,68 @@
+package com.stackroute.controller;
+
+import com.stackroute.domain.Product;
+import com.stackroute.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+
+@RestController
+@RequestMapping(value = "/api/v1")
+public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+
+    /**
+     * save a new Product
+     */
+    @PostMapping("/product")
+    public ResponseEntity<Product> saveProduct(@RequestBody Product product) {
+        Product savedProduct = productService.saveProduct(product);
+        return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+    }
+
+
+    /**
+     * retrieve all Products
+     */
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return new ResponseEntity<List<Product>>((List<Product>) productService.getAllProducts(), HttpStatus.OK);
+
+    }
+
+    /**
+     * retrieve Product by id
+     */
+    @GetMapping("product/{productId}")
+    public ResponseEntity<Product> getProductById(@PathVariable("productId") int productId) {
+        return new ResponseEntity<>(productService.getProductById(productId), HttpStatus.FOUND);
+    }
+
+    /**
+     * delete Product by id
+     */
+    @DeleteMapping("product/{productId}")
+    public ResponseEntity<Product> getProductAfterDeleting(@PathVariable("ProductId") int productId) {
+        return new ResponseEntity<>(productService.deleteProduct(productId), HttpStatus.OK);
+    }
+
+    /**
+     * update Product
+     */
+    @PutMapping("product")
+    public ResponseEntity<Product> updateProduct(@RequestBody Product product) {
+        Product updatedProduct = productService.updateProduct(product);
+        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+    }
+    
+}
