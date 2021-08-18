76749d335338d58b4a8812e4feb6370b99b74f45
jesse.tsang@cgi.com <jesse.tsang@cgi.com>
2021-08-18 01:57:11 -0400

    Remove extra space

--- productservice/src/main/java/com/stackroute/ProductServiceApplication.java
+++ productservice/src/main/java/com/stackroute/ProductServiceApplication.java
@@ -7,9 +7,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 public class ProductServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
-
 }
