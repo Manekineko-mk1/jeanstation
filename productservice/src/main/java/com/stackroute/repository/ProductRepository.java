1a84c644fd1f3d8c12a652041e7edda8f55a4a85
jesse.tsang@cgi.com <jesse.tsang@cgi.com>
2021-08-18 01:56:52 -0400

    Change repository extension
    From JPA -> MongoDB

--- productservice/src/main/java/com/stackroute/repository/ProductRepository.java
+++ productservice/src/main/java/com/stackroute/repository/ProductRepository.java
@@ -1,10 +1,10 @@
 package com.stackroute.repository;
 
 import com.stackroute.domain.Product;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, Integer> {
+public interface ProductRepository extends MongoRepository<Product, Integer> {
 
 }
