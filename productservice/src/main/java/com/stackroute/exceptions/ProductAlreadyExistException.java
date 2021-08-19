4542ee17e3088b122e3d4fd1d2807d4b2533a6b5
jesse.tsang@cgi.com <jesse.tsang@cgi.com>
2021-08-18 21:34:21 -0400

    Rename class

--- productservice/src/main/java/com/stackroute/exceptions/ProductAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.stackroute.exceptions;
-
-public class ProductAlreadyExistException extends RuntimeException {
-    public ProductAlreadyExistException(int productID) {
-        super(String.format("Product with ID %d already exist.", productID));
-    }
-}
