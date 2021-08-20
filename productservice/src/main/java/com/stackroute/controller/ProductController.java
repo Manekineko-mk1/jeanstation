3c9cb2f4adb245f3ee0b02b19965e83c693fa156
jesse.tsang@cgi.com <jesse.tsang@cgi.com>
2021-08-20 09:58:07 -0400

    Remove duplicate line

--- productservice/src/main/java/com/stackroute/controller/ProductController.java
+++ productservice/src/main/java/com/stackroute/controller/ProductController.java
@@ -14,7 +14,6 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/api/v1/")
 @Slf4j
