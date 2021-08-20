1c7137f24497379cf554b30860d9f2ab1ed5e843
jesse.tsang@cgi.com <jesse.tsang@cgi.com>
2021-08-20 12:34:01 -0400

    Refactor error handling

--- cartservice/src/main/java/com/stackroute/exceptions/CartNotFoundException.java
+++ cartservice/src/main/java/com/stackroute/exceptions/CartNotFoundException.java
@@ -1,7 +1,11 @@
 package com.stackroute.exceptions;
 
 public class CartNotFoundException extends RuntimeException{
-    public CartNotFoundException(int productID) {
+    public CartNotFoundException(String productID) {
         super(String.format("Cart with ID %d not found.", productID));
+    }
+
+    public CartNotFoundException() {
+        super();
     }
 }
