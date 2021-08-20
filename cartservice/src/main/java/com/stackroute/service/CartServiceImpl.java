f93cbe4fc03b833b66c913d69832a06d1f37aa5d
jesse.tsang@cgi.com <jesse.tsang@cgi.com>
2021-08-20 14:34:25 -0400

    Refactor and add logging to CartServiceImpl.java

--- cartservice/src/main/java/com/stackroute/service/CartServiceImpl.java
+++ cartservice/src/main/java/com/stackroute/service/CartServiceImpl.java
@@ -2,32 +2,56 @@ package com.stackroute.service;
 
 import com.stackroute.domain.Cart;
 
+import com.stackroute.exceptions.CartAlreadyExistException;
 import com.stackroute.repository.CartRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class CartServiceImpl implements CartService {
-    private final CartRepository cartRepository;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/uuuu - HH:mm:ss z");
 
     /**
      * Constructor based Dependency injection to inject CartRepository here
      */
     @Autowired
-    public CartServiceImpl(CartRepository cartRepository) {
-        this.cartRepository = cartRepository;
-    }
+    CartRepository cartRepository;
 
     /**
      * Implementation of saveCart method
      */
     @Override
     public Cart saveCart(Cart cart) {
-        boolean isCartExist = cartRepository.findById(cart.getId()).isPresent();
-        return cartRepository.save(cart);
+        boolean isCartExist;
+
+        if(cart.getId() == null) {
+            isCartExist = false;
+        } else {
+            isCartExist = cartRepository.findById(cart.getId()).isPresent();
+        }
+
+        ZonedDateTime zonedDateTimeNow = ZonedDateTime.now(ZoneId.of("America/Montreal"));
+        String timeStamp = zonedDateTimeNow.format(formatter);
+
+        if(isCartExist) {
+            log.error("ERROR: Unable to add cart. Cart already existed in database | Cart ID: {} | Timestamp(EST): {}",
+                    cart.getId(), timeStamp);
+
+            throw new CartAlreadyExistException(cart.getId());
+        } else {
+            log.info("SUCCESS: Add a product to the \"carts\" collection | Cart ID: {} | Timestamp(EST): {}",
+                    cart.getId(), timeStamp);
+
+            return cartRepository.save(cart);
+        }
     }
 
     /**
