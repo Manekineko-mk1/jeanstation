edd2155cc0529688320b6053274a1cbcbb1e1b35
jesse.tsang@cgi.com <jesse.tsang@cgi.com>
2021-08-23 23:53:12 -0400

    Connect MessengerService

--- jeanstationapp/src/app/product/product-item/product-item.component.ts
+++ jeanstationapp/src/app/product/product-item/product-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Product } from 'src/app/model/Product'
+import { Product } from 'src/app/model/Product';
+import { MessengerService } from 'src/app/services/messenger.service'
 
 @Component({
   selector: 'app-product-item',
@@ -10,9 +11,13 @@ export class ProductItemComponent implements OnInit {
 
   @Input() productItem: Product;
 
-  constructor() { }
+  constructor(private msg: MessengerService) { }
 
   ngOnInit(): void {
   }
 
+  // Handles when user click on "Add to Cart"
+  handleAddToCart() {
+    this.msg.sendMsg(this.productItem);
+  }
 }
