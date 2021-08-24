2484cd12ab6489cd933a0dc8d013c73822a127e8
jesse.tsang@cgi.com <jesse.tsang@cgi.com>
2021-08-23 23:47:33 -0400

    Implement cart component functionality

--- jeanstationapp/src/app/cart/cart.component.ts
+++ jeanstationapp/src/app/cart/cart.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Cart } from '../model/Cart';
-import { Product } from '../model/Product';
+import { Product } from 'src/app/model/Product'
 import { ApprouteService } from '../services/approute.service';
 import { CartService } from '../services/cart.service';
+import { MessengerService } from 'src/app/services/messenger.service';
+import { CartStatus } from 'src/app/model/CartStatus';
 
 @Component({
   selector: 'app-cart',
@@ -10,88 +12,113 @@ import { CartService } from '../services/cart.service';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  cartID: String;
+  cartStatus: CartStatus;
   cart:Cart;
-  cartItems:Product[];
+  cartItems: Product[];
+  tax=0.14;
+  priceTotalBeforeTax;
+  priceTotalAfterTax;
 
-  constructor(private cartservice:CartService, private approuter:ApprouteService) { }
+  constructor(private cartservice:CartService, private approuter:ApprouteService, private msg: MessengerService) { }
 
   ngOnInit(): void {
-    let priceTotal = 0;
+    if (this.cartStatus != null && this.cartStatus.status) {
+      this.retrieveCart(this.cartStatus.cartId);
+    } else {
+      this.initCart();
+    }
 
-    let payload = JSON.parse('{ "priceTotal" : 0}');
-
-    // Create an empty cart
-    this.createCart(payload);
-    this.cartItems = new Array<Product>();
-
-    this.cartItems.push({
-      "productName" : "ngOnInit Item",
-      "productDescription": "ngOnInit test",
-      "picture": "product.picture",
-      "priceCAD": 1,
-      "discount": 1,
-      "quantity": 1,
-      "productSize": "XL",
-      "productColor": "BLACK"
+    // Add listener to MessengerService
+    this.msg.getMsg().subscribe(product => {
+      this.addItemToCart(product);
+      this.calculateTotal();
     });
 
-    console.log('ngOnInit | cartItems: ' + JSON.stringify(this.cartItems));
     console.log('cartComponent initialized');
+  }
+
+  initCart() {
+    // Initialization
+    this.cart = JSON.parse('{ "priceTotalBeforeTax" : 0}');
+    this.priceTotalBeforeTax = 0;
+    this.priceTotalAfterTax = 0;
+    this.cartItems = new Array<Product>();
+
+    // Create an empty cart
+    this.createCart(this.cart);
+
+    this.cartStatus = new CartStatus(true, this.cart.id);
+
+    console.log('cartComponent initialized');
+  }
+
+  retrieveCart(cartId: string) {
+    this.cartservice.getCartById(cartId).subscribe(
+      data => {
+        this.cart = data;
+      }
+    );
   }
 
   createCart(cart:Cart) {
     this.cartservice.createCart(cart).subscribe(
       data => {
         this.cart = data;
-        this.cartID = data.id;
-        this.cartItems = data.cartItems;
-      }
-    );
-  }
-
-  updateCart(cart:Cart) {
-    this.cartservice.updateCart(cart).subscribe(
-      data => {
-        console.log('Updated cart: ' + data);
       }
     );
   }
 
   addItemToCart(product:Product) {
-    console.log('addItemToCart | product: ' + JSON.stringify(product));
-    console.log('addItemToCart | cartItems: ' + JSON.stringify(this.cartItems));
+    let ifProductExist = false;
 
-    this.cartItems.push({
-      "productName" : "test",
-      "productDescription": "test",
-      "picture": "test",
-      "priceCAD": 1,
-      "discount": 1,
-      "quantity": 1,
-      "productSize": "XL",
-      "productColor": "BLACK"
-    });
+    for(let i in this.cartItems) {
+      if(this.cartItems[i].id === product.id) {
+        this.cartItems[i].quantity++;
+        ifProductExist = true;
+        break;
+      }
+    }
+
+    if(!ifProductExist) {
+      product.quantity = 1;
+      this.cartItems.push(product);
+    }
 
     this.cart.cartItems = this.cartItems;
-
     this.updateCart(this.cart);
+  }
 
-    console.log('addItemToCart | cartItems: ' + JSON.stringify(this.cartItems));
-    console.log('Updated cart: ' + JSON.stringify(this.cart));
+  updateCart(cart:Cart) {
+    this.cartservice.updateCart(cart).subscribe(
+      data => {
+        console.log(data);
+        this.cart = data;
+      }
+    );
   }
 
   emptyCart() {
-    this.cart.priceTotal = 0;
-    this.cart.cartItems = [];
+    this.cart.priceTotalBeforeTax = 0;
+    this.cart.priceTotalAfterTax = 0;
+    this.cart.cartItems = new Array<Product>();
+
     this.cartservice.updateCart(this.cart);
   }
 
   deleteCart() {
-      this.cartservice.deleteCart(this.cartID).subscribe(
+      this.cartservice.deleteCart(this.cart.id).subscribe(
         data => {
           console.log('DeleteCart succeed: ' + data);
         }
       );
+  }
+
+  calculateTotal() {
+    this.cartItems.forEach(item => {
+      this.priceTotalBeforeTax = this.priceTotalBeforeTax + (item.quantity * item.price);
+
+    });
+
+    this.priceTotalAfterTax = this.priceTotalBeforeTax + (this.priceTotalAfterTax * this.tax);
   }
 }
