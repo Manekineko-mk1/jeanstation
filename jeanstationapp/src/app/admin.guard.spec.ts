d9b81e75de27eef94c84a94eb2fbcc9272739a52
jia.mo <jia.mo@cgi.com>
2021-08-27 14:17:07 -0400

    add e2e test

--- jeanstationapp/src/app/admin.guard.spec.ts
+++ jeanstationapp/src/app/admin.guard.spec.ts
@@ -1,21 +1,36 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AdminGuard } from './admin.guard';
+import { ApprouteService } from './services/approute.service';
 
 describe('AdminGuard', () => {
   let guard: AdminGuard;
+  let routerservice: ApprouteService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports : [ RouterTestingModule],
     });
     guard = TestBed.inject(AdminGuard);
+    routerservice = TestBed.get(ApprouteService);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
+  });
+
+  it('should hit route if logged in and is admin', () => {
+    routerservice.isAdmin.next(true);
+    routerservice.isLoggedIn.next(true);
+    expect(guard.canActivate(new ActivatedRouteSnapshot(), <RouterStateSnapshot>{url: 'testUrl'})).toBe(true);
+  });
+
+  it('should not hit route if not logged in or is not admin', () => {
+    routerservice.isAdmin.next(false);
+    expect(guard.canActivate(new ActivatedRouteSnapshot(), <RouterStateSnapshot>{url: 'testUrl'})).toBe(false);
   });
 });
--- jeanstationapp/src/app/admin/admin.component.spec.ts
+++ jeanstationapp/src/app/admin/admin.component.spec.ts
@@ -2,7 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { Product } from '../model/Product';
 import { ApprouteService } from '../services/approute.service';
 import { ProductService } from '../services/product.service';
@@ -73,12 +73,71 @@ describe('AdminComponent', () => {
     expect(component.addProduct).toBeTruthy();
   });
 
+  it('addProduct() should print error message if input is invalid', () => {
+    component.addProduct();
+    expect(component.message).toEqual('The fields should not be empty!!! Please verify details');
+  });
+
   it('addProduct() should add Product', () => {
     component.form.clearValidators();
     component.form.updateValueAndValidity();
     spyOn(service, 'addProduct').and.returnValue(of(product1));
     component.addProduct();
+    expect(service.addProduct).toHaveBeenCalled();
     expect(component.message).toEqual('Product added');
+  });
+
+  it('addProduct() should print error message if failed', () => {
+    component.form.clearValidators();
+    component.form.updateValueAndValidity();
+    spyOn(service, 'addProduct').and.returnValue(throwError({status:404}));
+    component.addProduct();
+    expect(service.addProduct).toHaveBeenCalled();
+    expect(component.message).toEqual('Registration failed!');
+  });
+
+  it('should have updateProduct()', () => {
+    expect(component.updateProduct).toBeTruthy();
+  });
+
+  it('should have deleteProduct()', () => {
+    expect(component.deleteProduct).toBeTruthy();
+  });
+
+  it('deleteProduct() should delete product', () => {
+    spyOn(service, 'deleteProduct').and.returnValue(of(product1));
+    component.deleteProduct("id");
+    expect(component.message).toEqual('Product deleted');
+  });
+
+  it('deleteProduct() should print error message if an error occurs', () => {
+    spyOn(service, 'deleteProduct').and.returnValue(throwError({status:404}));
+    component.deleteProduct("1");
+    expect(component.message).toEqual('Failed to delete product!');
+  });
+
+  it('should have clearForm()', () => {
+    expect(component.clearForm).toBeTruthy();
+  });
+
+  it('should have showUpdate()', () => {
+    expect(component.showUpdate).toBeTruthy();
+  });
+
+  it('should have addCategory()', () => {
+    expect(component.addCategory).toBeTruthy();
+  });
+
+  it('should have deleteCategory()', () => {
+    expect(component.deleteCategory).toBeTruthy();
+  });
+
+  it('should have triggerModal()', () => {
+    expect(component.triggerModal).toBeTruthy();
+  });
+
+  it('should have fileChange()', () => {
+    expect(component.fileChange).toBeTruthy();
   });
 
 });
