40f9d9b21a501cc96480afa4bc09a6dea92eaa6b
jesse.tsang@cgi.com <jesse.tsang@cgi.com>
2021-08-30 10:36:29 -0400

    WIP checkout.component.ts

--- jeanstationapp/src/app/checkout/checkout.component.ts
+++ jeanstationapp/src/app/checkout/checkout.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from '../model/Cart';
+import { Product } from 'src/app/model/Product';
+import { Money } from '../model/Money';
+import { CartService } from '../services/cart.service';
+import { CheckoutService } from '../services/checkout.service';
+import { CookieService } from 'ngx-cookie';
+import { MessengerService } from 'src/app/services/messenger.service';
 
 @Component({
   selector: 'app-checkout',
@@ -6,10 +13,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
+  cart: Cart;
+  cartId: string;
+  cartItems: Product[] = new Array<Product>();
+  tax = 0.14;
+  priceTotalBeforeTax: number = 0;
+  priceTotalAfterTax: number = 0;
 
-  constructor() { }
+  constructor(private cookieService: CookieService, private cartService:CartService,
+              private checkoutService:CheckoutService, private msg: MessengerService) { }
 
   ngOnInit(): void {
+    this.cartId = this.cookieService.get("cartId");
+
+    // Retrieve Cart from cartID exists in cookie ELSE create a new Cart
+    if (this.cartId != null) {
+      this.retrieveCart(this.cartId);
+    } else {
+      this.createCart();
+    }
+
+    this.handleSubscription();
+  }
+
+  retrieveCart(cartId: string) {
+    this.cartService.getCartById(cartId).subscribe(data => {
+        this.cart = data;
+        this.cart.cartItems === null ? this.cartItems = new Array<Product>() : this.cartItems = this.cart.cartItems;
+        this.priceTotalBeforeTax === null ? this.priceTotalBeforeTax = 0 : this.priceTotalBeforeTax = this.cart.priceTotalBeforeTax;
+        this.priceTotalAfterTax === null ? this.priceTotalAfterTax = 0 : this.priceTotalAfterTax = this.cart.priceTotalAfterTax;
+
+        console.log("retrieveCart@checkout")
+        console.log(this.cart);
+    });
+  }
+
+  createCart() {
+    // Create an empty cart
+    this.cart = JSON.parse('{ "priceTotalBeforeTax" : 0, "priceTotalAfterTax" : 0, "cartItems": [] }');
+
+    this.cartService.createCart(this.cart).subscribe(data => {
+      this.cart = data;
+      this.cookieService.put("cartId", this.cart.id);
+    });
+  }
+
+  handleSubscription() {
+    // Add listener to MessengerService
+    this.msg.getMsg().subscribe(product => {
+      this.calculateTotal();
+      this.retrieveCart(this.cartId);
+    });
+  }
+
+  calculateTotal() {
+    this.cartItems.forEach(item => {
+      this.priceTotalBeforeTax = this.priceTotalBeforeTax + (item.quantity * (item.price.amount / 100));
+    });
+
+    this.priceTotalAfterTax = this.priceTotalBeforeTax + (this.priceTotalAfterTax * this.tax);
   }
 
 }
