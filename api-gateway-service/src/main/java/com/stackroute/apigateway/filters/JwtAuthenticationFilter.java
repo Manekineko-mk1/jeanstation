1c9f78a487e5a99b3000b87f0410d930883cd40d
Hoa <kennan.hoa@cgi.com>
2021-08-31 12:41:13 -0400

    small fixes

--- api-gateway-service/src/main/java/com/stackroute/apigateway/filters/JwtAuthenticationFilter.java
+++ api-gateway-service/src/main/java/com/stackroute/apigateway/filters/JwtAuthenticationFilter.java
@@ -29,7 +29,7 @@ public class JwtAuthenticationFilter implements GatewayFilter {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = (ServerHttpRequest) exchange.getRequest();
 
-        final List<String> apiEndpoints = List.of("/register", "/login");
+        final List<String> apiEndpoints = List.of("/register", "/login", "/products", "/cart");
 
         Predicate<ServerHttpRequest> isApiSecured = r -> apiEndpoints.stream()
                 .noneMatch(uri -> r.getURI().getPath().contains(uri));
