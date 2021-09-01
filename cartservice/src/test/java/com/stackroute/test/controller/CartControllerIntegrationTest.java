d705c3e7b54c1022b7942fbb8cbfe9d34c7f01bf
jesse.tsang@cgi.com <jesse.tsang@cgi.com>
2021-09-01 17:54:45 -0400

    WIP not working

--- cartservice/src/test/java/com/stackroute/test/controller/CartControllerIntegrationTest.java
+++ cartservice/src/test/java/com/stackroute/test/controller/CartControllerIntegrationTest.java
@@ -1,69 +1,104 @@
 package com.stackroute.test.controller;
 
 import com.stackroute.domain.Cart;
+import com.stackroute.domain.Money;
+import com.stackroute.domain.Product;
+import com.stackroute.enums.Currency;
+import com.stackroute.exceptions.CartAlreadyExistException;
+import com.stackroute.exceptions.CartNotFoundException;
 import com.stackroute.service.CartService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
 
-@RestController
-@RequestMapping(value = "/api/v1")
+@SpringBootTest
+@Transactional
 public class CartControllerIntegrationTest {
-
-    private final CartService cartService;
-
     @Autowired
-    public CartControllerIntegrationTest(CartService cartService) {
-        this.cartService = cartService;
+    private CartService cartService;
+    private Cart cart;
+    private List<Product> cartItems;
+    int priceTotalBeforeTax;
+    int priceTotalAfterTax;
+
+    @BeforeEach
+    void setUp() {
+        cart = new Cart();
+        priceTotalBeforeTax = 100;
+        priceTotalAfterTax = 114;
+        cartItems = new ArrayList<Product>();
+        Product product = new Product();
+        cartItems.add(product);
     }
 
+    @AfterEach
+    void tearDown() {
+        cart = null;
+    }
 
-    /**
-     * save a new Cart
-     */
-    @PostMapping("/cart")
-    public ResponseEntity<Cart> saveCart(@RequestBody Cart cart) {
+    @Test
+    void givenProductToSaveThenShouldReturnTheSavedProduct() throws CartAlreadyExistException {
         Cart savedCart = cartService.saveCart(cart);
-        return new ResponseEntity<>(savedCart, HttpStatus.CREATED);
+        assertNotNull(savedCart);
+        assertEquals(cart.getId(), savedCart.getId());
     }
 
-
-    /**
-     * retrieve all Carts
-     */
-    @GetMapping("/carts")
-    public ResponseEntity<List<Cart>> getAllCarts() {
-        return new ResponseEntity<>(cartService.findAllCarts(), HttpStatus.OK);
-
+    @Test
+    void givenProductToSaveThenThrowException() throws CartAlreadyExistException {
+        assertNotNull(cartService.saveCart(cart));
+        assertThrows(CartAlreadyExistException.class, () -> cartService.saveCart(cart));
     }
 
-    /**
-     * retrieve Cart by id
-     */
-    @GetMapping("cart/{cartId}")
-    public ResponseEntity<Cart> getCartById(@PathVariable("cartId") String cartId) {
-        return new ResponseEntity<>(cartService.findCartById(cartId), HttpStatus.FOUND);
+    @Test
+    void givenProductToDeleteThenShouldReturnTheDeletedProduct() throws CartNotFoundException {
+        assertNotNull(cartService.saveCart(cart));
+        Cart deletedCart = cartService.deleteCartById(cart.getId());
+        assertNotNull(deletedCart);
     }
 
-    /**
-     * delete Cart by id
-     */
-    @DeleteMapping("cart/{cartId}")
-    public ResponseEntity<Cart> getCartAfterDeleting(@PathVariable("CartId") String cartId) {
-        return new ResponseEntity<>(cartService.deleteCartById(cartId), HttpStatus.OK);
+    @Test
+    void givenProductToDeleteThenThrowException() throws CartNotFoundException {
+        assertThrows(CartNotFoundException.class, () -> cartService.deleteCartById(cart.getId()));
     }
 
-    /**
-     * update Cart
-     */
-    @PutMapping("cart")
-    public ResponseEntity<Cart> updateCart(@RequestBody Cart cart) {
-        Cart updatedCart = cartService.updateCart(cart);
-        return new ResponseEntity<>(updatedCart, HttpStatus.OK);
+    @Test
+    void givenCallToGetAllProductsThenListShouldNotBeNull() throws Exception {
+        List<Cart> retrievedProducts = cartService.findAllCarts();
+        assertNotNull(retrievedProducts);
     }
-    
+
+    @Test
+    void givenProductToUpdateThenShouldReturnUpdatedProduct() throws CartNotFoundException {
+        Cart savedCart = cartService.saveCart(cart);
+        savedCart.setPriceTotalBeforeTax(10);
+        Cart updatedCart = cartService.updateCart(savedCart);
+        assertNotNull(updatedCart);
+        assertEquals(10, updatedCart.getPriceTotalBeforeTax());
+    }
+
+    @Test
+    void givenProductToUpdateThenThrowException() throws CartNotFoundException {
+        assertThrows(CartNotFoundException.class, () -> cartService.updateCart(cart));
+    }
+
+    @Test
+    void givenProductIdThenShouldReturnRespectiveProduct() throws CartNotFoundException {
+        assertNotNull(cartService.saveCart(cart));
+        Cart retrievedCart = cartService.findCartById(cart.getId());
+        assertNotNull(retrievedCart);
+    }
+
+    @Test
+    void givenProductIdThenShouldThrowException() throws CartNotFoundException {
+        assertThrows(CartNotFoundException.class, () -> cartService.findCartById(cart.getId()));
+    }
 }
 
