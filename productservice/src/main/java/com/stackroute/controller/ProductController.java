d534b8a50d7a9a8191636a20cc8bf5d8a6791b27
jesse.tsang@cgi.com <jesse.tsang@cgi.com>
2021-09-07 13:08:09 -0400

    Update ProductController and tests

--- productservice/src/main/java/com/stackroute/controller/ProductController.java
+++ productservice/src/main/java/com/stackroute/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.stackroute.domain.Product;
 import com.stackroute.service.ProductService;
 import io.swagger.annotations.ApiOperation;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
